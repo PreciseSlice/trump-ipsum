70c247a3c599a7f72fb8b44864d550e8a5186508
Matthew Renn <rennmatthewp@gmail.com>
2018-03-27 13:57:53 -0600

    initial mirgration

---
+++ db/migrations/20180327133429_initial.js
@@ -0,0 +1,26 @@
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('remarks', table => {
+      table.increments('id').primary();
+      table.string('title');
+      table.string('topic');
+      table.string('date');
+      table.timestamps(true, true);
+    }),
+    knex.schema.createTable('paragraphs', table => {
+      table.increments('id').primary();
+      table.string('length');
+      table.string('text');
+      table.integer('remarks_id').unsigned();
+      table.foreign('remarks_id').references('remarks.id');
+      table.timestamps(true, true);
+    })
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('paragraphs'),
+    knex.schema.dropTable('remarks')
+  ]);
+};
